dac34c0c7b9753082eb3875e29ee4a50b3d94744
kurokikaze <kurokikaze@yandex.ru>
2013-03-19 19:58:49 +0400

    Removed hardcoded UI stuff from step

--- step.js
+++ step.js
@@ -1,13 +1,7 @@
 var step = function(name) {
     this.name = name;
     this.mandate_actions = [];
-    this.element = $('<span/>').attr('id','step_' + this.name.toLowerCase()).text(this.name);
-    $('#steps').append(this.element);
     this.activate = function() {
-        // Clear all steps
-        $('#steps span').removeClass('active');
-        // Set current step in UI
-        this.element.addClass('active');
         // Apply mandatory actions
         for (var i = 0; i <= this.mandate_actions.length; i++) {
             if (this.mandate_actions[i] && typeof this.mandate_actions[i] == 'function') {
